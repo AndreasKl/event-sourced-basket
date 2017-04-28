b62df2bdaaa8d0a37c9554dfed8daadd8aa8f069
Andreas Kluth <mail@andreaskluth.net>
2017-04-29 00:16:45 +0200

    Moved from memory to JPA store, no config needed as the auto config just takes care of all settings.

--- src/main/java/net/andreaskluth/basket_service/ApplicationConfiguration.java
+++ src/main/java/net/andreaskluth/basket_service/ApplicationConfiguration.java
@@ -1,15 +1,6 @@
 package net.andreaskluth.basket_service;
 
-import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.eventhandling.saga.repository.SagaStore;
-import org.axonframework.eventhandling.saga.repository.inmemory.InMemorySagaStore;
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-import org.axonframework.messaging.interceptors.BeanValidationInterceptor;
 import org.axonframework.spring.config.AxonConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,29 +10,9 @@ import net.andreaskluth.basket_service.basket.commands.BasketCommandHandler;
 @Configuration
 public class ApplicationConfiguration {
 
-    @Autowired
-    private AxonConfiguration axonConfiguration;
-
     @Bean
-    public BasketCommandHandler commandHandler(EventStore eventStore) {
+    public BasketCommandHandler commandHandler(AxonConfiguration axonConfiguration) {
         return new BasketCommandHandler(axonConfiguration.repository(Basket.class));
-    }
-
-    @Bean
-    public CommandBus simpleCommandBus() {
-        SimpleCommandBus simpleCommandBus = new SimpleCommandBus();
-        simpleCommandBus.registerDispatchInterceptor(new BeanValidationInterceptor<>());
-        return simpleCommandBus;
-    }
-
-    @Bean
-    public EventStorageEngine eventStorageEngine() {
-        return new InMemoryEventStorageEngine();
-    }
-
-    @Bean
-    public SagaStore<Object> sagaStore() {
-        return new InMemorySagaStore();
     }
 
 }
