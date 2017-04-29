612dcf19d999f0126e61d7642fa2d74e3948a1f1
Andreas Kluth <mail@andreaskluth.net>
2017-04-29 15:23:14 +0200

    Added postgres instead of h2.

--- src/main/java/net/andreaskluth/basket_service/basket/query/ReadBasket.java
+++ src/main/java/net/andreaskluth/basket_service/basket/query/ReadBasket.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -18,7 +19,7 @@ public class ReadBasket {
     @Id
     public String id;
 
-    @OneToMany(cascade = { CascadeType.ALL }, orphanRemoval = true)
+    @OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER, orphanRemoval = true)
     public Set<ReadLineItem> lineItems = new HashSet<>();
 
     ReadBasket() {
